72ffdece450135463dfc201eacdc4de1d4c367ce
safa <safayasmin567@gmail.com>
2026-07-16 19:28:39 +0530

    trafic light using react

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
-    const [color,setcolor]=useState("white");
+    const [signel ,setsignel]=useState("black");
 
     useEffect(()=>{
-      const colors=["white","green","red","yellow"];
-      var index=0;
-      const interval=setInterval(() => {
-        index=(index+1 )%colors.length;
-        setcolor(colors[index]);
-      }, 2000);
-      return ()=>clearInterval(interval);
+         const color=["green","red","yellow"];
+         var index=0;
+        const interval=setInterval(() => {
+            index=(index+1)%color.length;
+            setsignel(color[index]);
+        },2000);
+        return ()=>clearInterval(interval);
     },[])
-
   return (
+    <div className='w-[100px] h-[200px] flex flex-col items-center justify-center gap-3 m-8 bg-gray-500'>
     <div
+    className='w-[30px] h-[30px] rounded-full'
     style={{
-        backgroundColor:color
-    }}
-    className='h-screen'
-    >
-        <h1>{color}</h1> 
+       backgroundColor:signel==="green"?"green":"black"
+    }}></div>
+
+    <div
+    className='w-[30px] h-[30px] rounded-full'
+    style={{
+       backgroundColor:signel==="red"?"red":"black"
+    }}></div>
+
+    <div
+    className='w-[30px] h-[30px] rounded-full'
+    style={{
+        backgroundColor:signel==="yellow"?"yellow":"black"
+    }}></div>
+
+      
     </div>
   )
 }
 
 export default Todo
+
+
