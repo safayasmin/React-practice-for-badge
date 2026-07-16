06fde0c43a983f54367ac7b7a9ebc1c72963554a
safa <safayasmin567@gmail.com>
2026-07-16 18:43:27 +0530

    color change using setinterval

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,37 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Todo = () => {
     const [color,setcolor]=useState("white");
 
-    const greenclick=()=>{
-        setcolor("green")
-    }
+    useEffect(()=>{
+      const colors=["white","green","red","yellow"];
+      var index=0;
+      const interval=setInterval(() => {
+        index=(index+1 )%colors.length;
+        setcolor(colors[index]);
+      }, 2000);
+      return ()=>clearInterval(interval);
+    },[])
 
-    const yellowclick=()=>{
-        setcolor("yellow")
-    }
-
-    const redclick=()=>{
-        setcolor("red")
-    }
-
-
-    const whiteclick=()=>{
-        setcolor("white")
-    }
-    
   return (
     <div
-    style={{backgroundColor:color}}
+    style={{
+        backgroundColor:color
+    }}
     className='h-screen'
     >
-        <h1 className='text-2xl font-bold tracking-[2px]'>Color change </h1>
-
-        <button onClick={greenclick} className='border py-2 px-3 m-3'>Green</button>
-        <button onClick={yellowclick} className='border py-2 px-3 ml-3'>yellow</button>
-        <button onClick={redclick} className='border py-2 px-3 ml-3'>red</button>
-        <button onClick={whiteclick} className='border py-2 px-3 ml-3'>white</button>
-      
+        <h1>{color}</h1> 
     </div>
   )
 }
