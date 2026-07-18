ffb8b7fbb11851e81056e2a279831305949da7e8
safa <safayasmin567@gmail.com>
2026-07-18 18:22:35 +0530

    solve a problem using useRef()

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,115 +1,19 @@
-
-
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import React, { useRef } from 'react'
 
 const Todo = () => {
-  const [name,setname]=useState("");
-  const [fulldata,setfulldata]=useState([]);
-  const [trash,settrash]=useState([]);
+  const count=useRef(0);
 
-  useEffect(()=>{
-    getdata();
-    gettrash();
-  },[]);
-
-  const gettrash=async()=>{
-    const res=await axios.get("http://localhost:5000/trash");
-    settrash(res.data);
-  }
-  const getdata=async()=>{
-    const res=await axios.get("http://localhost:5000/users");
-    setfulldata(res.data);
-  }
-  
-  const senddata=async()=>{
-    const handledata={
-      name:name
-    }
-    await axios.post(`http://localhost:5000/users`,handledata);
-    setname("");
-    getdata();
-  }
-
-  const handledelete=async(id)=>{
-    const deleteuser=fulldata.find((item)=>item.id===id);
-    const res=await axios.post(`http://localhost:5000/trash`,deleteuser);
-    await axios.delete(`http://localhost:5000/users/${id}`);
-    
-    getdata();
-    gettrash();
-  }
-
-  const handleundo=async(item)=>{
-    await axios.post(`http://localhost:5000/users`,{
-      name:item.name,
-    });
-    await axios.delete(`http://localhost:5000/trash/${item.id}`);
-    getdata();
-    gettrash();
+  const handlecount=()=>{
+    count.curent=count.curent+1;
+    console.log(count.current)
   }
   return (
     <div>
-      <h1 className='m-2 text-2xl font-bold tracking-[3px] uppercase'>Todo</h1>
-      <input
-      type='text'
-      placeholder='enter name'
-      value={name}
-      onChange={(e)=>setname(e.target.value)}
-      className='border border-black-500 py-2 px-2 m-2'
-      />
-      <button
-      onClick={()=>senddata()}
-      className='border py-2 px-2'
-      >add</button>
-
-    
-      {fulldata.map((n)=>(
-        <div key={n.id}>
-          <h1 className='text-xl font-bold '>{n.name}</h1>
-
-          <button onClick={()=>handledelete(n.id)}
-            className='border py-1 px-1 bg-red-500 mb-7'
-          >delete</button>
-        </div>
-      ))}
+      <h1 className='text-2xl font-bold'>ref is</h1>
+      <button className='border py-4 px-3' onClick={handlecount}>add</button>
       
-        <hr />
-        <h1 className='text-2xl font-bold'>trash</h1>
-
-        <br /><hr />
-        {trash.map((n)=>(
-          <div key={n.id}>
-            <h1 className='text-2xl font-bold'>{n.name}</h1>
-            <button onClick={()=>handleundo(n)}className='border py-2 px-2 bg-blue-500 mb-7'>undo</button>
-          </div>
-        ))}     
     </div>
   )
 }
 
 export default Todo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
